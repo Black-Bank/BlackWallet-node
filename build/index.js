aecfe45609e9e8393a0ec7233046d8d902147b79
Cyro56 <cyrorenato56@gmail.com>
2022-10-20 18:50:15 -0300

    refactor: create logic uri

--- build/index.js
+++ build/index.js
@@ -6,13 +6,15 @@ const apollo_server_1 = require("apollo-server");
 const type_graphql_1 = require("type-graphql");
 const WalletResolver_1 = require("./resolvers/WalletResolver");
 async function bootstrap() {
+    const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 4000;
     const server = new apollo_server_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
             resolvers: [WalletResolver_1.WalletResolver],
         }),
         introspection: true,
     });
-    server.listen(Number.parseInt(process.env.PORT) || 4000);
+    server.listen(PORT);
+    console.log("Server: " + PORT);
 }
 exports.bootstrap = bootstrap;
 bootstrap();
--- src/database/findWallets.ts
+++ src/database/findWallets.ts
@@ -1,34 +1,33 @@
 export async function FindWallets(HashId: string, key: string) {
   const mongodb = require("mongodb").MongoClient;
-  const url = `mongodb+srv://${key}@cluster0.im4zqou.mongodb.net/?retryWrites=true&w=majority`;
-  const herokuURI = process.env.MONGODB_URI;
+  const url = process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : `mongodb+srv://${key}@cluster0.im4zqou.mongodb.net/?retryWrites=true&w=majority`;
+
   let result = [];
 
   function data() {
     return new Promise<void>((resolve) => {
-      mongodb.connect(
-        herokuURI || url,
-        (erro: { message: string }, banco: any) => {
-          if (erro) {
-            throw erro;
-          }
-          const dbo = banco.db("CFBcursos");
-          let query = { idHash: HashId };
-
-          dbo
-            .collection("colecao")
-            .find(query)
-            .toArray(async (erro, resultado) => {
-              if (erro) {
-                throw erro;
-              }
-              const res = resultado;
-              result.push(res[0].carteiras);
-              resolve();
-              banco.close();
-            });
+      mongodb.connect(url, (erro: { message: string }, banco: any) => {
+        if (erro) {
+          throw erro;
         }
-      );
+        const dbo = banco.db("CFBcursos");
+        let query = { idHash: HashId };
+
+        dbo
+          .collection("colecao")
+          .find(query)
+          .toArray(async (erro, resultado) => {
+            if (erro) {
+              throw erro;
+            }
+            const res = resultado;
+            result.push(res[0].carteiras);
+            resolve();
+            banco.close();
+          });
+      });
     });
   }
 
