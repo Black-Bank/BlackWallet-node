c7ef20a492d67c815949b56ed50649feef944925
Cyro56 <cyrorenato56@gmail.com>
2022-10-29 21:26:18 -0300

    refactor: remove log

--- build/database/balance/removeBalance.js
+++ build/database/balance/removeBalance.js
@@ -2,25 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RemoveBalance = void 0;
 function RemoveBalance(HashId, key) {
-    const mongodb = require("mongodb").MongoClient;
-    const url = `mongodb+srv://${key}@cluster0.im4zqou.mongodb.net/?retryWrites=true&w=majority`;
-    mongodb.connect(url, (erro, banco) => {
+  const mongodb = require("mongodb").MongoClient;
+  const url = `mongodb+srv://${key}@cluster0.im4zqou.mongodb.net/?retryWrites=true&w=majority`;
+  mongodb.connect(url, (erro, banco) => {
+    if (erro) {
+      throw erro;
+    }
+    const dbo = banco.db("BlackNodeDB");
+    let query = { idHash: HashId };
+    dbo
+      .collection("TotalBalance")
+      .updateOne(query, { $pop: { month: 1 } }, async (erro, resultado) => {
         if (erro) {
-            throw erro;
+          console.log(erro);
+          throw erro;
         }
-        const dbo = banco.db("BlackNodeDB");
-        let query = { idHash: HashId };
-        dbo
-            .collection("TotalBalance")
-            .updateOne(query, { $pop: { month: 1 } }, async (erro, resultado) => {
-            if (erro) {
-                console.log(erro);
-                throw erro;
-            }
-            console.log(resultado);
-            console.log("Balance removido");
-            banco.close();
-        });
-    });
+        console.log("Balance removido");
+        banco.close();
+      });
+  });
 }
 exports.RemoveBalance = RemoveBalance;
