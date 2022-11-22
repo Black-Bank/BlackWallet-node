ac0d6e2c0c8b2feb8d27b83c778dca10cfb596ad
Cyro56 <cyrorenato56@gmail.com>
2022-11-22 00:56:30 -0300

    new querys

--- build/resolvers/BalanceResolver.js
+++ build/resolvers/BalanceResolver.js
@@ -11,6 +11,9 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BalanceResolver = void 0;
 const type_graphql_1 = require("type-graphql");
@@ -18,9 +21,31 @@ const findBalance_1 = require("../database/balance/findBalance");
 const insertBalance_1 = require("../database/balance/insertBalance");
 const removeBalance_1 = require("../database/balance/removeBalance");
 const balance_1 = require("../entities/balance");
+const axios_1 = __importDefault(require("axios"));
 let BalanceResolver = class BalanceResolver {
     async getBalance(key, HashId) {
         return await (0, findBalance_1.FindBalance)(HashId, key);
+    }
+    async getPrice(coin, apiKey) {
+        let priceInfo = 0;
+        if (coin === "BTC") {
+            const getPrice_url = `https://chain.so/api/v2/get_price/BTC/USD`;
+            const response = await axios_1.default.get(getPrice_url);
+            let exchangeArray = response === null || response === void 0 ? void 0 : response.data.data.prices;
+            exchangeArray.map((data) => (priceInfo += Number(data.price)));
+            return Number((priceInfo / exchangeArray.length).toFixed(2));
+        }
+        else if (coin === "ETH") {
+            const getPrice_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+            const response = await axios_1.default.get(getPrice_url, {
+                headers: {
+                    "X-CMC_PRO_API_KEY": apiKey,
+                },
+            });
+            let coinInfo = response.data.data.filter((data) => data.symbol === coin);
+            let coinPrice = Number(coinInfo[0].quote.USD.price.toFixed(2));
+            return coinPrice;
+        }
     }
     RemoveBalance(key, HashId, removeOption) {
         (0, removeBalance_1.RemoveBalance)(HashId, key, removeOption);
@@ -40,6 +65,14 @@ __decorate([
     __metadata("design:paramtypes", [String, String]),
     __metadata("design:returntype", Promise)
 ], BalanceResolver.prototype, "getBalance", null);
+__decorate([
+    (0, type_graphql_1.Query)(() => Number),
+    __param(0, (0, type_graphql_1.Arg)("coin")),
+    __param(1, (0, type_graphql_1.Arg)("apiKey")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String]),
+    __metadata("design:returntype", Promise)
+], BalanceResolver.prototype, "getPrice", null);
 __decorate([
     (0, type_graphql_1.Mutation)(() => Boolean),
     __param(0, (0, type_graphql_1.Arg)("key")),
--- src/resolvers/BalanceResolver.ts
+++ src/resolvers/BalanceResolver.ts
@@ -3,6 +3,8 @@ import { FindBalance } from "../database/balance/findBalance";
 import { InsertBalance } from "../database/balance/insertBalance";
 import { RemoveBalance } from "../database/balance/removeBalance";
 import { Balance } from "../entities/balance";
+import axios from "axios";
+import { Console } from "console";
 
 @Resolver()
 export class BalanceResolver {
@@ -12,6 +14,33 @@ export class BalanceResolver {
     @Arg("HashId") HashId: string
   ): Promise<Balance> {
     return await FindBalance(HashId, key);
+  }
+  @Query(() => Number)
+  async getPrice(
+    @Arg("coin") coin: string,
+    @Arg("apiKey") apiKey: string
+  ): Promise<number> {
+    let priceInfo = 0;
+    if (coin === "BTC") {
+      const getPrice_url = `https://chain.so/api/v2/get_price/BTC/USD`;
+      const response = await axios.get(getPrice_url);
+      let exchangeArray = response?.data.data.prices;
+      exchangeArray.map((data) => (priceInfo += Number(data.price)));
+
+      return Number((priceInfo / exchangeArray.length).toFixed(2));
+    } else if (coin === "ETH") {
+      const getPrice_url =
+        "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+      const response = await axios.get(getPrice_url, {
+        headers: {
+          "X-CMC_PRO_API_KEY": apiKey,
+        },
+      });
+      let coinInfo = response.data.data.filter((data) => data.symbol === coin);
+      let coinPrice = Number(coinInfo[0].quote.USD.price.toFixed(2));
+
+      return coinPrice;
+    }
   }
   @Mutation(() => Boolean)
   RemoveBalance(
