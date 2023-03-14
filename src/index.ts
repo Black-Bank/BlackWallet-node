d46113fa1bca21c8df2355e65c689d9f3b30e144
Cyro Renato <86036682+Cyro56@users.noreply.github.com>
2023-03-13 22:13:53 -0300

    create update pass mutation (#4)

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { WalletResolver } from "./resolvers/WalletResolver";
 import { BalanceResolver } from "./resolvers/BalanceResolver";
+import { AuthResolver } from "./resolvers/LoginResolver";
 
 export async function bootstrap() {
   const server = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [WalletResolver, BalanceResolver],
+      resolvers: [WalletResolver, BalanceResolver, AuthResolver],
     }),
     introspection: true,
   });
