4a025f38ba3071b8218776f947e088d3fc12e46c
Cyro Renato <86036682+Cyro56@users.noreply.github.com>
2023-04-03 16:00:23 -0300

    create loggin structure (#8)

--- src/resolvers/LoginResolver.ts
+++ src/resolvers/LoginResolver.ts
@@ -1,9 +1,24 @@
 import { Resolver, Query, Mutation, Arg, Args } from "type-graphql";
 import { Cypher } from "../Domain/cypher";
 import { InsertCypher } from "../Domain/InsertCypher";
+import { InsertUser } from "../Domain/InsertUser";
 
 @Resolver()
 export class AuthResolver {
+  @Mutation(() => Boolean)
+  async CreateUser(
+    @Arg("key") key: string,
+    @Arg("Email") Email: string,
+    @Arg("passWord") passWord: string
+  ): Promise<boolean> {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const hashedPassword = Cypher(passWord);
+    if (regex.test(Email)) {
+      return await InsertUser(Email, key, hashedPassword);
+    } else {
+      throw new Error(`Invalid email`);
+    }
+  }
   @Mutation(() => Boolean)
   async UpdatePass(
     @Arg("key") key: string,
