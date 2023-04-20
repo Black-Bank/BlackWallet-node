7854010043f446a1b5b75fe43193af294615c0ba
Cyro56 <cyrorenato56@gmail.com>
2023-04-20 09:24:33 -0300

    fix: build error

--- build/Domain/AuthUser.js
+++ build/Domain/AuthUser.js
@@ -6,33 +6,38 @@ const path = require("path");
 const dotenvPath = path.resolve(__dirname, "../../.env");
 require("dotenv").config({ path: dotenvPath });
 async function AuthUser(Email, password) {
-    const mongodb = require("mongodb").MongoClient;
-    const url = `mongodb+srv://CreditBlack:${process.env.KEY_SECRET_MONGODB}@cluster0.yfsjwse.mongodb.net/?retryWrites=true&w=majority`;
-    const passwordAuth = password.substring(0, password.indexOf(process.env.PASSWORD_EARLY));
-    const passwordAuth = password.substring(0, password.indexOf(process.env.PASSWORD_EARLY));
-    function data() {
-        return new Promise((resolve) => {
-            mongodb.connect(url, (erro, banco) => {
-                if (erro) {
-                    throw erro;
-                }
-                const dbo = banco.db("userInfo");
-                dbo
-                    .collection("master")
-                    .findOne({ Email: Email }, async (erro, resultado) => {
-                    if (erro) {
-                        throw erro;
-                    }
-                    bcrypt.compare(passwordAuth, resultado.senha, function (err, AuthResponse) {
-                        if (err)
-                            throw err;
-                        resolve(Boolean(AuthResponse));
-                    });
-                    banco.close();
-                });
-            });
-        });
-    }
-    return await data();
+  const mongodb = require("mongodb").MongoClient;
+  const url = `mongodb+srv://CreditBlack:${process.env.KEY_SECRET_MONGODB}@cluster0.yfsjwse.mongodb.net/?retryWrites=true&w=majority`;
+  const passwordAuth = password.substring(
+    0,
+    password.indexOf(process.env.PASSWORD_EARLY)
+  );
+  function data() {
+    return new Promise((resolve) => {
+      mongodb.connect(url, (erro, banco) => {
+        if (erro) {
+          throw erro;
+        }
+        const dbo = banco.db("userInfo");
+        dbo
+          .collection("master")
+          .findOne({ Email: Email }, async (erro, resultado) => {
+            if (erro) {
+              throw erro;
+            }
+            bcrypt.compare(
+              passwordAuth,
+              resultado.senha,
+              function (err, AuthResponse) {
+                if (err) throw err;
+                resolve(Boolean(AuthResponse));
+              }
+            );
+            banco.close();
+          });
+      });
+    });
+  }
+  return await data();
 }
 exports.AuthUser = AuthUser;
