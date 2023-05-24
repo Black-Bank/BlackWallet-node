f16ad6951c6f79399c0f3e8bfb9362af3ea48566
Cyro56 <cyro.costa@ztech.net>
2023-05-24 13:36:11 -0300

    feat: teste em prod

--- src/Domain/getCoinPrice.ts
+++ src/Domain/getCoinPrice.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function CoinPrice(coin: string) {
   try {
-    const getPrice_url = `https://api.binance.com/api/v3/avgPrice?symbol=${coin}USDT`;
+    const getPrice_url = `https://api.binance.us/api/v3/avgPrice?symbol=${coin}USDT`;
     const response = await axios.get(getPrice_url);
     const coinPrice = Number(response?.data.price);
 
